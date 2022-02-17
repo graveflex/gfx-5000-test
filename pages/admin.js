6da330c186a00b345ef7a08c1d2e92df8eadd742
Will McCracken <wem5637@gmail.com>
2022-02-16 23:26:06 -0600

    client and admin

--- pages/admin.js
+++ pages/admin.js
@@ -4,12 +4,25 @@ import styles from '../styles/Home.module.css'
 import React, {useEffect, useState} from 'react';
 import Hls from "hls.js";
 import axios from "axios"
+import { io } from "socket.io-client";
+// const API_BASE_URL = "http://64.227.30.198"
+const API_BASE_URL = "localhost"
+
+const socket = io(API_BASE_URL);
 
 export default function AdminPanel(props) {
   const [streams, setStreams] = useState([])
+  const [channel, setChannel] = useState(null)
+
+  useEffect(()=>{
+    axios.get("http://64.227.30.198:3000")
+    .then(({data})=>{
+      setChannel(data.name)
+    })
+  }, [])
 
   useEffect(() => {
-    axios.get("http://64.227.30.198:8000/api/streams")
+    axios.get("API_BASE_URL:8000/api/streams")
     .then(({ data }) => {
       var streams = data.live
       const sts = Object.keys(streams).map((stream, i)=>{
@@ -19,18 +32,21 @@ export default function AdminPanel(props) {
     })
   }, [])
 
-  const VideoStream = ({name, index}) => {
+  const VideoStream = ({streamName, index}) => {
     useEffect(()=>{
       var video = document.getElementById(`player${index}`)
       const hls = new Hls()
-      const url = `http://64.227.30.198:8000/live/${name}/index.m3u8`
+      const url = `API_BASE_URL:8000/live/${streamName}/index.m3u8`
       hls.loadSource(url);
       hls.attachMedia(video);
     }, [])
+
     return <div>
         <div>Static Stream {index}</div>
         <video id={`player${index}`} controls style={{height:"100px", width:"180px"}}></video>
-        <button>Set as active</button>
+        <button onClick={()=>{
+          socket.emit("switch_stream", streamName)
+        }}>Set as active</button>
       </div>
   }
 
@@ -39,9 +55,9 @@ export default function AdminPanel(props) {
       <h2>Admin Panel</h2>
       <div data-vjs-player>
       {streams.map((stream)=>{
-        return <div style={{display:"inline-block", margin: "10px"}} key={`t${stream.index}`}>
+        return <div style={{display:"inline-block", margin: "10px", border: stream.name == channel ? "5px solid red" : "1px solid black"}} key={`t${stream.index}`}>
           <div>{stream.name}</div>
-          <VideoStream controls style={{height:"100px", width:"180px"}} name={stream.name} index={stream.index}/>
+          <VideoStream controls style={{height:"100px", width:"180px"}} streamName={stream.name} index={stream.index}/>
         </div>
       })}
       </div>
--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,42 @@ import styles from '../styles/Home.module.css'
 import React, {useEffect, useState} from 'react';
 import Hls from "hls.js";
 import axios from "axios"
+import { io } from "socket.io-client";
+const API_BASE_URL = "http://64.227.30.198"
+const socket = io(API_BASE_URL);
 
 export default function Client(props) {
+  const [channel, setChannel] = useState(null)
+
+  useEffect(()=>{
+    io.on('switch_stream', (name)=>{
+      setChannel(name)
+    })
+    axios.get("http://64.227.30.198:3000")
+    .then(({data})=>{
+      setChannel(data.name)
+    })
+  }, [])
+
+  const VideoStream = ({streamName, index}) => {
+    useEffect(()=>{
+      var video = document.getElementById(`player${index}`)
+      const hls = new Hls()
+      const url = `API_BASE_URL:8000/live/${streamName}/index.m3u8`
+      hls.loadSource(url);
+      hls.attachMedia(video);
+    }, [streamName])
+
+    return <div>
+        <video id={`player${index}`} controls style={{height:"300px", width:"640px"}}></video>
+      </div>
+  }
 
   return (
     <div style={{margin:"30px"}}> 
       <h2>Client Panel</h2>
       <div data-vjs-player>
-      test
+        {channel?<VideoStream streamName={channel} index={0}/>:<div>Loading channel...</div>}
       </div>
     </div>
   )
